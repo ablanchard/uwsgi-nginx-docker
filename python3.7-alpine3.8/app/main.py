d4b839498482009fd85c30b99801483f49824891
ablanchard <alexandre.blanchard@protonmail.com>
2019-05-02 00:35:13 +0200

    Use gunicorn

--- python3.7-alpine3.8/app/main.py
+++ python3.7-alpine3.8/app/main.py
@@ -1,10 +1,8 @@
 import sys
 
+def app(env, start_response):
+	version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+	start_response("200 OK", [("Content-Type", "text/plain")])
+	message = "Hello World from a default Python {} app in a Docker container, with Meinheld and Gunicorn on Alpine (default)".format(version)
+	return [message.encode("utf-8")]
 
-def application(env, start_response):
-    version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    message = "Hello World from a default Nginx uWSGI Python {} app in a Docker container in Alpine (default)".format(
-        version
-    )
-    return [message.encode("utf-8")]
